3c86742618a8023b3fede1f3e70c99e643f5e2c0
Aivars Smaukstelis <aivars.smaukstelis@gmail.com>
2021-05-04 17:20:11 +0300

    ontermediary js folder is not actually needed. bundle.js is compiled and served by web server

--- app/app.ts
+++ app/app.ts
@@ -1,7 +1,7 @@
 function startGame() {
     // starting game (will beremoved with tsconfig.json ->"removeComments": true
     var messageElement = document.getElementById('messages');
-    messageElement!.innerText = 'Starting New Game...'
+    messageElement!.innerText = 'Starting New Game...123'
 }
 
 document.getElementById('startGame')!.addEventListener('click', startGame);
