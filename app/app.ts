8c4e6ce8f9e52d6826cda0b2f28d30e9741277cf
Aivars Smaukstelis <aivars.smaukstelis@gmail.com>
2021-05-05 00:34:20 +0300

    extracting random learning code

--- app/app.ts
+++ app/app.ts
@@ -1,4 +1,5 @@
 /// <reference path="./player.ts" />
+/// <reference path="./zzz.ts" />
 function startGame() {
     // starting game (will be removed with tsconfig.json ->"removeComments": true
 
@@ -9,7 +10,7 @@ function startGame() {
 }
 
 document.getElementById('startGame')!.addEventListener('click', startGame);
-document.getElementById('randomStuff')!.addEventListener('click', doRandomStuff);
+document.getElementById('randomStuff')!.addEventListener('click', Zzz.doRandomStuff);
 
 function logPlayer(name: string = 'Unknown MultiMath player') {
     console.log(`New game starting for player: ${name}`)
@@ -47,141 +48,3 @@ const logError = (message: string) => console.error(message)
 
 const firstPlayer: Player = new Player('Aivars')
 console.log(`firstPlayer: ${firstPlayer.name}`)
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-//---------------------------------------
-
-function doRandomStuff() {
-    writeToScore()
-    logMe()
-    strictCode()
-    typeAssertion()
-    optionalValues()
-    callFatArrow()
-    ifaces()
-}
-
-function writeToScore() {
-    const STARTING_NEW_GAME = 'Starting New Game...'
-    let messageElement = document.getElementById('messages')
-    messageElement!.innerText = STARTING_NEW_GAME
-}
-
-function logMe() {
-    //logPlayer(playerName) would not compile with strict option
-    let playerName: string = "Aivars"
-    logPlayer(playerName)
-}
-
-function strictCode() {
-    let basicString: string;
-    basicString = 'a'
-    //basicString = null; does not compile with "strict": true,
-    let nullableString: string | null;
-    nullableString = 'b'
-
-    console.log(`sc basic: ${basicString}; nullable: ${nullableString} tp: ${typeFor(nullableString)} tp_strange: ${strangeTypeFor(nullableString)}`)
-    nullableString = null
-    console.log(`sc basic: ${basicString}; nullable: ${nullableString} tp: ${typeFor(nullableString)} tp_strange (object???): ${strangeTypeFor(nullableString)}`)
-}
-
-function typeFor(elem: any) {
-    if(typeof elem ==='string') {
-        return "str"
-    } else if(typeof elem ==='number') {
-        return "nr"
-    } else if(elem === null) {
-        return "null"
-    } else {
-        return typeof elem
-    }
-}
-
-function strangeTypeFor(elem: any) {
-    if(typeof elem ==='string') {
-        return "str"
-    } else {
-        return typeof elem
-    }
-}
-
-function typeAssertion() {
-    let legacyCodeValue: any = 5;
-    let fixedTypeValue: string = (<number>legacyCodeValue).toFixed(4)
-    console.log(`legacy: ${legacyCodeValue} fixed: ${fixedTypeValue}`)
-    let anotherApproach: string = (legacyCodeValue as number).toFixed(4)
-    console.log(`legacy: ${legacyCodeValue} another fixed: ${anotherApproach}`)
-}
-
-function optionalValues() {
-    console.log(`withOptional missing:${withReturnValue(1)}`);
-}
-
-function withReturnValue(score: number, optionalMessage?: string, paramWithDefault: number = -123): string {
-    return `score: ${score}, msg: ${optionalMessage}, param: ${paramWithDefault}`
-}
-
-function callFatArrow() {
-    console.log(`2 squared = ${arrowSquare(2)}`)
-    console.log(`2 + 3 = ${arrowAdd(2,3)}`)
-    arrowGreeting()
-    arrowFilter()
-}
-
-const arrowSquare = (x:number) => x * x
-
-const arrowAdd = (a:number, b:number) => a + b
-
-const arrowGreeting = () => console.log("Hi!")
-
-function arrowFilter() {
-    let scores: number[] = [70,110,10]
-    let highScores: number[] = scores.filter((value, index, array) => {
-        console.log(`---value:${value},index:${index},array:${array}`)
-        if (value > 100) {
-            return true
-        }
-    })
-    console.log(`sc: ${scores} - hs: ${highScores}`)
-}
-
-function ifaces() {
-    interface Employee {
-        name: string
-        title: string
-        company: string
-    }
-
-    interface ArtCritic {
-        name: string
-        gere: string
-    }
-
-    let me = {
-        name: 'Aivars',
-        title: 'Coder',
-        level: `over 9000`,
-        company: 'Secret'
-    }
-
-    let employedDeveloper: Employee = me
-    console.log(`employedDeveloper ${employedDeveloper.name}`)
-
-    //let artCriticDeveloper: ArtCritic = me  does not compile
-}
---
+++ app/zzz.ts
@@ -0,0 +1,123 @@
+class Zzz {
+
+    static doRandomStuff() {
+        Zzz.writeToScore()
+        Zzz.logMe()
+        Zzz.strictCode()
+        Zzz.typeAssertion()
+        Zzz.optionalValues()
+        Zzz.callFatArrow()
+        Zzz.ifaces()
+    }
+
+    static writeToScore() {
+        const STARTING_NEW_GAME = 'Starting New Game...'
+        let messageElement = document.getElementById('messages')
+        messageElement!.innerText = STARTING_NEW_GAME
+    }
+
+
+    static logMe() {
+        //logPlayer(playerName) would not compile with strict option
+        let playerName: string = "Aivars"
+        logPlayer(playerName)
+    }
+
+    static strictCode() {
+        let basicString: string;
+        basicString = 'a'
+        //basicString = null; does not compile with "strict": true,
+        let nullableString: string | null;
+        nullableString = 'b'
+
+        console.log(`sc basic: ${basicString}; nullable: ${nullableString} tp: ${this.typeFor(nullableString)} tp_strange: ${this.strangeTypeFor(nullableString)}`)
+        nullableString = null
+        console.log(`sc basic: ${basicString}; nullable: ${nullableString} tp: ${this.typeFor(nullableString)} tp_strange (object???): ${this.strangeTypeFor(nullableString)}`)
+    }
+
+    static typeFor(elem: any) {
+        if(typeof elem ==='string') {
+            return "str"
+        } else if(typeof elem ==='number') {
+            return "nr"
+        } else if(elem === null) {
+            return "null"
+        } else {
+            return typeof elem
+        }
+    }
+
+    static strangeTypeFor(elem: any) {
+        if(typeof elem ==='string') {
+            return "str"
+        } else {
+            return typeof elem
+        }
+    }
+
+    static typeAssertion() {
+        let legacyCodeValue: any = 5;
+        let fixedTypeValue: string = (<number>legacyCodeValue).toFixed(4)
+        console.log(`legacy: ${legacyCodeValue} fixed: ${fixedTypeValue}`)
+        let anotherApproach: string = (legacyCodeValue as number).toFixed(4)
+        console.log(`legacy: ${legacyCodeValue} another fixed: ${anotherApproach}`)
+    }
+
+    static optionalValues() {
+        console.log(`withOptional missing:${this.withReturnValue(1)}`);
+    }
+
+    static withReturnValue(score: number, optionalMessage?: string, paramWithDefault: number = -123): string {
+        return `score: ${score}, msg: ${optionalMessage}, param: ${paramWithDefault}`
+    }
+
+    static callFatArrow() {
+        console.log(`2 squared = ${this.arrowSquare(2)}`)
+        console.log(`2 + 3 = ${this.arrowAdd(2,3)}`)
+        this.arrowGreeting()
+        this.arrowFilter()
+    }
+
+    static arrowSquare = (x:number) => x * x
+
+    static arrowAdd = (a:number, b:number) => a + b
+
+    static arrowGreeting = () => console.log("Hi!")
+
+    static arrowFilter() {
+        let scores: number[] = [70,110,10]
+        let highScores: number[] = scores.filter((value, index, array) => {
+            console.log(`---value:${value},index:${index},array:${array}`)
+            if (value > 100) {
+                return true
+            }
+        })
+        console.log(`sc: ${scores} - hs: ${highScores}`)
+    }
+
+    static ifaces() {
+        interface Employee {
+            name: string
+            title: string
+            company: string
+        }
+
+        interface ArtCritic {
+            name: string
+            gere: string
+        }
+
+        let me = {
+            name: 'Aivars',
+            title: 'Coder',
+            level: `over 9000`,
+            company: 'Secret'
+        }
+
+        let employedDeveloper: Employee = me
+        console.log(`employedDeveloper ${employedDeveloper.name}`)
+
+        //let artCriticDeveloper: ArtCritic = me  does not compile
+    }
+
+}
