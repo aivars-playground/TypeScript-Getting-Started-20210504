03de58c2bc4449a77bf0c63992fc0a08ca8df85d
Aivars Smaukstelis <aivars.smaukstelis@gmail.com>
2021-05-04 18:04:07 +0300

    fix order

--- app/app.ts
+++ app/app.ts
@@ -1,15 +1,16 @@
 function startGame() {
     // starting game (will be removed with tsconfig.json ->"removeComments": true
 
+    const playerName: string = "Aivars"
     logPlayer(playerName)
-    var playerName: string = "Aivars"
+
 
     const STARTING_NEW_GAME = 'Starting New Game...'
     let messageElement = document.getElementById('messages');
     messageElement!.innerText = STARTING_NEW_GAME
 }
 
-function logPlayer(name) {
+function logPlayer(name: string) {
     console.log(`New game starting for player: ${name}`)
 }
 
